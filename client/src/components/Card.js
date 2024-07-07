93049da830d5afebe82115d61a7657428de2b476
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-07-06 23:29:41 -0500

    Fixed tailwind, reordered card component

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -21,23 +21,26 @@ const Card = ({ card }) => {
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-yellow-200">
-      <img className="w-full" src="https://media.bleacherreport.com/image/upload/w_800,h_533,c_fill/v1696219301/udsc6iawctp8ygwfvebn.jpg" alt="Handcuffed Card" />
+
+      {/* Card Name */}
+      <div className="font-bold text-xl mb-2">
+        {card.name}
+      </div>
+
+      {/* Card Type */}
+      <p className="text-gray-700 text-base">
+        {formatCardType()}
+      </p>
+
+      {/* Fortitude */}
+      <p className="text-gray-700 text-base mt-2">
+        Fortitude: {card.fortitude}
+      </p>
+
+      {/* Card Image */}
+      <img className="w-full" src="https://media.bleacherreport.com/image/upload/w_800,h_533,c_fill/v1696219301/udsc6iawctp8ygwfvebn.jpg" alt={"Picture for the " + card.name.toString() + "card."} />
+      
       <div className="px-6 py-4">
-        {/* Card Name */}
-        <div className="font-bold text-xl mb-2">
-          {card.name}
-        </div>
-
-        {/* Card Type */}
-        <p className="text-gray-700 text-base">
-          {formatCardType()}
-        </p>
-
-        {/* Fortitude */}
-        <p className="text-gray-700 text-base mt-2">
-          Fortitude: {card.fortitude}
-        </p>
-
         {/* Card Text */}
         <p className="text-gray-700 text-base mt-2">
           {card.cardText}
@@ -53,6 +56,7 @@ const Card = ({ card }) => {
       <div className="px-6 pt-4 pb-2">
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Damage: {card.damage}</span>
       </div>
+
     </div>
   );
 };
